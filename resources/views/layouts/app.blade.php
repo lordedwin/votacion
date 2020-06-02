233f0aef1fa6875030f60df7b04d378056222d27
lordedwin <lordedwin@users.noreply.github.com>
2020-06-02 11:28:19 -0500

    Actualizacion meta name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,13 +12,13 @@
 
 
   <!--  Social tags      -->
-  <meta name="keywords" content="creative tim, html dashboard, html css dashboard, web dashboard, bootstrap 4 dashboard, bootstrap 4, css3 dashboard, bootstrap 4 admin, now ui dashboard bootstrap 4 dashboard, frontend, responsive bootstrap 4 dashboard, now ui design, now ui dashboard bootstrap 4 dashboard">
-  <meta name="description" content="Now UI Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
+  <meta name="keywords" content="votacion 2020, modulo de votacion">
+  <meta name="description" content="Nuevo modulo de votacion 2020.">
 
 
   <!-- Schema.org markup for Google+ -->
-  <meta itemprop="name" content="Now Ui Dashboard PRO by Creative Tim">
-  <meta itemprop="description" content="Now UI Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
+  <meta itemprop="name" content="votacion 2020, modulo de votacion">
+  <meta itemprop="description" content="Nuevo modulo de votacion 2020.">
 
   <meta itemprop="image" content="https://s3.amazonaws.com/creativetim_bucket/products/72/opt_nudp_thumbnail.jpg">
 
@@ -28,19 +28,19 @@
   <meta name="twitter:site" content="@creativetim">
   <meta name="twitter:title" content="[Now Ui Dashboard PRO] by Creative Tim">
 
-  <meta name="twitter:description" content="Now UI Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
+  <meta name="twitter:description" content="Nuevo modulo de votacion 2020..">
   <meta name="twitter:creator" content="@creativetim">
   <meta name="twitter:image" content="https://s3.amazonaws.com/creativetim_bucket/products/72/opt_nudp_thumbnail.jpg">
 
 
   <!-- Open Graph data -->
   <meta property="fb:app_id" content="655968634437471">
-  <meta property="og:title" content="Now Ui Dashboard PRO by Creative Tim" />
+  <meta property="og:title" content="Nuevo modulo de votacion 2020." />
   <meta property="og:type" content="article" />
   <meta property="og:url" content="http://demos.creative-tim.com/now-ui-dashboard-pro/examples/dashboard.html" />
   <meta property="og:image" content="https://s3.amazonaws.com/creativetim_bucket/products/72/opt_nudp_thumbnail.jpg"/>
-  <meta property="og:description" content="Now UI Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you." />
-  <meta property="og:site_name" content="Creative Tim" />
+  <meta property="og:description" content="Nuevo modulo de votacion 2020." />
+  <meta property="og:site_name" content="Edwin Rojas" />
   <title>
     Votaciones 2020
   </title>
